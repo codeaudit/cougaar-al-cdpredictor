f04ebe4d59927b63225e9202ebe1637aa7889729
wpeng <wpeng>
2001-10-30 21:39:42 +0000

    Added jar file generation

--- castellan/test/bin/build.bat
+++ castellan/test/bin/build.bat
@@ -1,7 +1,9 @@
-set CASTELLANHOME=S:\psu_stuff\hydra
-set CASTELLANSRC=%CASTELLANHOME%/src
+set CASTELLANHOME=C:\castellan\bin
+REM This is the root directory where the hydra branch is stored
+set CASTELLANROOT=C:\cvslocal\hydra
+set CASTELLANSRC=%CASTELLANROOT%\src
 set COUGAARPATH=%COUGAAR_INSTALL_PATH%
-set LIBPATHS=%COUGAARPATH%/lib/core.jar
+set LIBPATHS=%COUGAARPATH%\lib\core.jar
 set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/lib/build.jar
 set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/sys/xerces.jar
 set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/lib/glm.jar
@@ -13,4 +15,7 @@ set SOURCEFILES=%CASTELLANSRC%/org/hydra/server/*.java %CASTELLANSRC%/org/hydra/
 set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/hydra/pdu/*.java %CASTELLANSRC%/org/hydra/libui/*.java %CASTELLANSRC%/org/hydra/util/*.java
 set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/hydra/planlog/*.java %CASTELLANSRC%/org/dbinterface/*.java %CASTELLANSRC%/org/hydra/plugin/*.java %CASTELLANSRC%/org/hydra/pspace/search/*.java
 
-javac -classpath %LIBPATHS% -d %CASTELLANHOME%/bin %SOURCEFILES%
+javac -classpath %LIBPATHS% -d %CASTELLANHOME% %SOURCEFILES%
+mkdir %CASTELLANHOME%\org\dbinterface\defs
+copy %CASTELLANROOT%\data\defs\*.xml %CASTELLANHOME%\org\dbinterface\defs
+jar cvf %COUGAARPATH%\lib\castellan.jar %CASTELLANHOME%\org
