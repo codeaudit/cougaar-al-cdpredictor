12c0704a5e726e0e5b25ef5b0506f61189929bf9
cvspsu <cvspsu>
2002-05-22 23:28:58 +0000

    *** empty log message ***

--- castellan/test/bin/RunHydra.bat
+++ castellan/test/bin/RunHydra.bat
@@ -1,4 +1,10 @@
-set CASTELLANHOME=S:\hydra
-set LIBPATH=%COUGAAR_INSTALL_PATH%\sys\castellan.jar;%COUGAAR_INSTALL_PATH%\lib\core.jar;%COUGAAR_INSTALL_PATH%\lib\glm.jar;%COUGAAR_INSTALL_PATH%\sys\grappa1_2_bbn.jar;%CLASSPATH%
-set LIBPATH=%COUGAAR_INSTALL_PATH%\sys\xerces.jar;%COUGAAR_INSTALL_PATH%\sys\mm-mysql-2.jar;%LIBPATH%;%COUGAAR_INSTALL_PATH%\sys\jgl3.1.0.jar
-java -Xmx164m -Xss2m -Dcastellanhome=%CASTELLANHOME% -cp %LIBPATH% org.hydra.server.ServerApp
+set CASTELLANHOME=C:\cvslocal\hydra
+set LIBPATH=%COUGAAR_INSTALL_PATH%/lib/castellan.jar;
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/lib/core.jar;
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/lib/glm.jar;
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/sys/grappa1_2.jar;
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/sys/xerces.jar;
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/sys/mm-mysql-2.jar;
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/sys/jgl3.1.0.jar
+set LIBPATH=%LIBPATH%;%COUGAAR_INSTALL_PATH%/sys/matlib.jar
+java -Xmx164m -Xss2m -Dcastellanhome=%CASTELLANHOME% -cp %LIBPATH% org.cougaar.tools.castellan.server.ServerApp
