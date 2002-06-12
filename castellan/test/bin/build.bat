b977135768e3efb197cdbdb4942065631b5953c0
wpeng <wpeng>
2002-06-12 15:09:50 +0000

    *** empty log message ***

--- castellan/test/bin/build.bat
+++ castellan/test/bin/build.bat
@@ -1,7 +1,7 @@
-set TMPDIR=C:\software\castellan\tmpdir
 REM This is the root directory where the hydra branch is stored
 set CASTELLANROOT=C:\software\castellan
 set CASTELLANSRC=%CASTELLANROOT%\src
+set TMPDIR=%CASTELLANROOT%\tmpdir
 set COUGAARPATH=%COUGAAR_INSTALL_PATH%
 
 set LIBPATHS=%COUGAARPATH%\lib\core.jar
@@ -14,6 +14,7 @@ set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/sys/jgl3.1.0.jar
 set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/sys/grappa1_2.jar
 set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/sys/matlib.jar
 set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/sys/mm-mysql-2.jar
+set LIBPATHS=%LIBPATHS%;%COUGAARPATH%/lib/aggagent.jar
 
 echo off
 set SOURCEFILES=%CASTELLANSRC%/org/cougaar/tools/castellan/server/*.java 
@@ -27,7 +28,12 @@ set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/castellan/util/*.
 set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/castellan/planlog/*.java 
 set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/castellan/plugin/*.java 
 set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/castellan/pspace/search/*.java
+set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/castellan/tests/*.java 
+set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/castellan/batching/*.java
 set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/alf/*.java
+set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/alf/sensor/*.java
+set SOURCEFILES=%SOURCEFILES% %CASTELLANSRC%/org/cougaar/tools/alf/sensor/plugin/*.java
+
 echo on
 javac -classpath %LIBPATHS% -d %TMPDIR% %SOURCEFILES%
 REM copy %CASTELLANROOT%\data\defs\*.xml %TMPDIR%\org\dbinterface\defs
