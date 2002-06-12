d133403fe8692a3afc29f8c3f86cc2df67f2e167
wpeng <wpeng>
2002-06-12 16:33:55 +0000

    *** empty log message ***

--- castellan/src/org/cougaar/tools/castellan/tests/RelayTestPlugin.java
+++ castellan/src/org/cougaar/tools/castellan/tests/RelayTestPlugin.java
@@ -29,6 +29,8 @@ import org.cougaar.core.service.BlackboardService;
 import org.cougaar.core.service.UIDService;
 import org.cougaar.core.service.UIDServer;
 import org.cougaar.core.adaptivity.InterAgentCondition;
+import org.cougaar.core.adaptivity.SensorCondition;
+import org.cougaar.core.adaptivity.OMCRangeList;
 import org.cougaar.core.relay.Relay;
 import org.cougaar.core.util.UID;
 import org.cougaar.core.mts.MessageAddress;
@@ -36,6 +38,7 @@ import org.cougaar.core.component.ServiceBroker;
 import org.cougaar.core.blackboard.IncrementalSubscription;
 import org.cougaar.core.agent.ClusterIdentifier;
 import org.cougaar.util.UnaryPredicate;
+import org.cougaar.tools.alf.sensor.SensorConditionRelay;
 
 import java.util.Set;
 import java.util.Collections;
@@ -43,110 +46,25 @@ import java.util.Vector;
 import java.util.Iterator;
 
 public class RelayTestPlugin extends ComponentPlugin {
-    public static class MyTestRelay implements Relay.Source, Relay.Target {
-        public MyTestRelay(UID uid, MessageAddress target, MessageAddress source, Object content, Object response ) {
-            this.uid = uid;
-            this.source= source ;
-            this.value = content ;
-            this.response = response ;
-            targets = ((target != null) ?
-             Collections.singleton(target) :
-             Collections.EMPTY_SET);
-        }
-
-        public String toString() {
-            return "<MyTestRelay: Uid= " + uid + ", value=" + getValue() + ",source=" + getSource() + ",target=" + targets + ">" ;
-        }
-
-        public Set getTargets() {
-            return targets ;
-        }
-
-        public UID getUID() {
-            return uid ;
-        }
-
-        public MessageAddress getSource() {
-            return source ;
-        }
-
-        public void setValue( Object o ) {
-            value = o ;
-        }
-
-        public Object getValue() {
-            return value ;
-        }
-
-        public Object getContent() {
-            return value;
-        }
-
-        public void setUID(UID uid) {
-            throw new RuntimeException( "Tried to set UID" ) ;
-        }
-
-        public Object getResponse() {
-            return null;
-        }
-
-        public Relay.TargetFactory getTargetFactory() {
-            return SimpleRelayFactory.INSTANCE ;
-        }
-
-        public int updateContent(Object o, Relay.Token token) {
-            value = o ;
-            return Relay.CONTENT_CHANGE ;
-        }
-
-        public int updateResponse(MessageAddress address, Object o) {
-            return 0;
-        }
-
-        private static final class SimpleRelayFactory
-        implements TargetFactory, java.io.Serializable {
-
-          public static final SimpleRelayFactory INSTANCE =
-            new SimpleRelayFactory();
-
-          private SimpleRelayFactory() {}
-
-          public Relay.Target create(
-              UID uid,
-              MessageAddress source,
-              Object content,
-              Token token) {
-            return new MyTestRelay( uid, null, source, content, null ) ;
-          }
-
-          private Object readResolve() {
-            return INSTANCE;
-          }
-        };
-
-
-        protected UID uid ;
-        protected Set targets ;
-        protected MessageAddress source ;
-        protected Object value ;
-        protected Object response ;
-
-    }
 
     protected void setupSubscriptions() {
         BlackboardService bs = getBlackboardService() ;
         ServiceBroker sb = getServiceBroker() ;
         UIDService us = (UIDService ) sb.getService( this, UIDService.class, null ) ;
 
+        // This is stupid.
+        OMCRangeList range = new OMCRangeList( new Integer( 0 ), new Integer( 1 ) ) ;
+
+        SensorCondition condition = new SensorCondition( "FallingBehind", range, new Integer( 0 ) ) ;
 
         Vector v = new Vector( getParameters() )  ;
-        mt = new MyTestRelay(us.nextUID(), new ClusterIdentifier( ( String ) v.get(0) ), getClusterIdentifier(),
-                getClusterIdentifier().cleanToString(), null ) ;
+        mt = new SensorConditionRelay(us.nextUID(), new ClusterIdentifier( ( String ) v.get(0) ), getClusterIdentifier(),
+                condition, null ) ;
         bs.publishAdd( mt ) ;
 
         relays = ( IncrementalSubscription ) bs.subscribe( new UnaryPredicate() {
             public boolean execute(Object o) {
-                if ( o instanceof MyTestRelay ) {
+                if ( o instanceof SensorConditionRelay ) {
                     return true ;
                 }
                 return false ;
@@ -158,13 +76,11 @@ public class RelayTestPlugin extends ComponentPlugin {
     protected void execute() {
 
         for ( Iterator iter = relays.getAddedCollection().iterator() ;  iter.hasNext() ; ) {
-            System.out.println("Found relay in " + getClusterIdentifier() + "= "  + iter.next() );
+            System.out.println(getBindingSite().getAgentIdentifier() + "::Found relay in " + getClusterIdentifier() + "= "  + iter.next() );
         }
 
-        mt.setValue( "Moose" );
-        getBlackboardService().publishChange( mt ) ;
     }
 
-    MyTestRelay mt ;
+    SensorConditionRelay mt ;
     IncrementalSubscription relays ;
 }
