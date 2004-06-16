9ecab8746ce1662d2331803e8e65e290ac7314a6
cvspsu <cvspsu>
2004-06-16 21:47:24 +0000

    *** empty log message ***

--- cpe/src/org/cougaar/cpe/model/TargetGenerator.java
+++ cpe/src/org/cougaar/cpe/model/TargetGenerator.java
@@ -7,4 +7,14 @@ public interface TargetGenerator
     public void initialize( Document doc ) ;
 
     public void execute( WorldState ws ) ;
+
+    /**
+     * Reset the time base.  For example, if we generate a number of targets into the "future"
+     * (e.g. in order to generate a number of initial targets) and then reset the time to zero
+     * prior to the beginning of execution, we should reinitialize any internal time depedent
+     * variables.
+     *
+     * @param time
+     */
+    public void resetTime( long time ) ;
 }
