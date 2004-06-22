3aae9b9f91d3d94117ff18ff48fa4995f3a13b15
cvspsu <cvspsu>
2004-06-22 23:11:05 +0000

    *** empty log message ***

--- cpe/src/org/cougaar/cpe/ui/MPObserver.java
+++ cpe/src/org/cougaar/cpe/ui/MPObserver.java
@@ -1,15 +1,3 @@
-/*
-* This software contains OpenCybele(TM) software and Cybele(TM) software.
-* OpenCybele(TM) software and Cybele(TM) software,
-* copyright © 2001-2003 Intelligent Automation, Inc.
-*
-* This software has been delivered with unlimited rights in accordance with
-* FAR 52.227-14 (Rights in Data-General) to Raytheon Company and to NASA
-* under Contract Number NAS2-00015, and Raytheon and the US Government
-* are free to use, reproduce, create derivative works, display and disclose the
-* software without restriction provided that this copyright notice appears in all copies.
-*/
-
 package org.cougaar.cpe.ui;
 
 import org.cougaar.tools.techspecs.qos.MeasurementPoint;
