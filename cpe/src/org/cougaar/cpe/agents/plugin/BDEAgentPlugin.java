0aeb9808dfdb5a2baa71c2fd8e3faeb6fb32c2f8
cvspsu <cvspsu>
2004-07-06 19:59:36 +0000

    Added measurement point for violations.

--- cpe/src/org/cougaar/cpe/agents/plugin/BDEAgentPlugin.java
+++ cpe/src/org/cougaar/cpe/agents/plugin/BDEAgentPlugin.java
@@ -1,3 +1,24 @@
+/*
+ * <copyright>
+ *  Copyright 2003-2004 Intelligent Automation, Inc.
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
 package org.cougaar.cpe.agents.plugin;
 
 import org.cougaar.core.adaptivity.OMCRangeList;
@@ -503,12 +524,13 @@ public class BDEAgentPlugin extends ComponentPlugin implements MessageSink {
 
             MeasuredWorldMetrics mwm;
             referenceZoneWorld.addEventListener( mwm = new MeasuredWorldMetrics( agg.getId(),
-                    referenceZoneWorld, 40000 ) );
+                    referenceZoneWorld, 5000 ) );
             bnAggUnitToMetricsMap.put( agg.getId(), mwm ) ;
             getBlackboardService().publishAdd( mwm.getAttrition() ) ;
             getBlackboardService().publishAdd( mwm.getEntryRate() ) ;
             getBlackboardService().publishAdd( mwm.getKills() ) ;
             getBlackboardService().publishAdd( mwm.getPenalties() ) ;
+            getBlackboardService().publishAdd( mwm.getViolations() ) ;
 
 			// Make configuration message and send it to the subordinate.
 			if (subordinateCombatOrganizations.get(agg.getId()) == null) {
