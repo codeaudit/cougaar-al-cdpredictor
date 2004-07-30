5565b91efc958c242126aa3aef4d3d9304420393
cvspsu <cvspsu>
2004-07-30 02:59:03 +0000

    control message class

---
+++ cpe/src/org/cougaar/cpe/agents/messages/ControlMessage.java
@@ -0,0 +1,44 @@
+package org.cougaar.cpe.agents.messages;
+
+import org.cougaar.cpe.model.UnitEntity;
+import org.cougaar.tools.techspecs.events.MessageEvent;
+import java.util.HashMap;
+
+/**
+ * User: Nathan Gnanasambandam
+ * Date: July 15, 2004
+ * Time: 11:03:30 AM
+ */
+public class ControlMessage extends MessageEvent {
+
+	public ControlMessage( String entityName,  String controlSet ) {
+		setValue( controlSet );
+		this.entityName = entityName;
+	}
+
+	public String getControl() {
+		return (String) getValue() ;
+	}
+
+	public String getEntityName() {
+		return entityName;
+	}
+	
+	public Object getControlParameter(String key) {
+		if (controlSet.get(key)!=null)
+			return controlSet.get(key);
+		else return null;
+	}
+	
+	public void putControlParameter(Object key, Object value){
+		controlSet.put(key,value);
+	}
+	
+	public void empty(){
+		controlSet.clear();	
+	}	
+
+	protected String entityName ;
+	protected HashMap controlSet= new HashMap(); 	
+		
+}
