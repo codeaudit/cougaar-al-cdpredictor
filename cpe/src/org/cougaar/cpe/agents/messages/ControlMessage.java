eec470e7408da76f2a07adb9a744d8441a3d2180
cvspsu <cvspsu>
2004-09-28 18:44:40 +0000

    added some more methods

--- cpe/src/org/cougaar/cpe/agents/messages/ControlMessage.java
+++ cpe/src/org/cougaar/cpe/agents/messages/ControlMessage.java
@@ -3,6 +3,7 @@ package org.cougaar.cpe.agents.messages;
 import org.cougaar.cpe.model.UnitEntity;
 import org.cougaar.tools.techspecs.events.MessageEvent;
 import java.util.HashMap;
+import org.cougaar.core.mts.MessageAddress;
 
 /**
  * User: Nathan Gnanasambandam
@@ -11,34 +12,46 @@ import java.util.HashMap;
  */
 public class ControlMessage extends MessageEvent {
 
-	public ControlMessage( String entityName,  String value ) {
-		setValue( value );
+	public ControlMessage(String entityName, String value) {
+		setValue(value);
 		this.entityName = entityName;
 	}
 
 	public String getControl() {
-		return (String) getValue() ;
+		return (String) getValue();
 	}
 
 	public String getEntityName() {
 		return entityName;
 	}
-	
-	public Object getControlParameter(String key) {
-		if (controlSet.get(key)!=null)
-			return controlSet.get(key);
-		else return null;
-	}
-	
-	public void putControlParameter(Object key, Object value){
-		controlSet.put(key,value);
-	}
-	
-	public void empty(){
-		controlSet.clear();	
-	}	
 
-	protected String entityName ;
-	protected HashMap controlSet= new HashMap(); 	
-		
+	public Object getControlParameter(MessageAddress agentName, String key) {
+		if (controlSet.containsKey((Object) agentName)) {
+			HashMap h = (HashMap) controlSet.get((Object) agentName);
+			if (h.containsKey((Object) key)) {
+				return (h.get((Object) key));
+			}
+		}
+		return null;
+	}
+
+	public void putControlParameter(Object key, Object value) {
+		controlSet.put(key, value);
+	}
+
+	public void empty() {
+		controlSet.clear();
+	}
+
+	public void paramString(StringBuffer buf) {
+		buf.append(controlSet.toString());
+	}
+
+	public void putControlSet(HashMap h) {
+		this.controlSet = h;
+	}
+
+	protected String entityName;
+	protected HashMap controlSet = new HashMap();
+
 }
